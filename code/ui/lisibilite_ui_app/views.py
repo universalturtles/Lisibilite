6e1fbb926ad2d5c37b33a27284c161b3636e3e06
Shyam Mohan Kizhakekara <shyammohank29@gmail.com>
2020-06-01 14:29:49 +0100

    Issue #16 Updated the file with code comments

--- code/ui/lisibilite_ui_app/views.py
+++ code/ui/lisibilite_ui_app/views.py
@@ -4,23 +4,48 @@ from lisibilite.Lisibilite import Lisibilite
 
 
 class HomePageView(View):
+
 	def get(self, request):
+		"""
+		The function to handle the HTTP GET request for home page.
+		:param request: The django HTTP request object
+		:return The render object
+		"""
 		return render(request, 'homepage.html')
 
 	def post(self, request):
+		"""
+		The function to handle the HTTP POST request for Home Page.
+		:param request: The django HTTP request object
+		:return None
+		"""
 		return None
 
 
 class UserInputPageView(View):
+
 	def get(self, request):
+		"""
+		The function to handle the HTTP GET request for text input page.
+		:param request: The django HTTP request object
+		:return The render object
+		"""
 		return render(request, 'userinputpage.html')
 
 	def setOutputValuesToSession(self, request, outputModel):
+		"""
+		The function to set the data from output model as session variables.
+		:param request: The django HTTP request object
+		:param outputModel: The lisibilite output model
+		:return None
+		"""
 		coreMetrics = outputModel.coreMetrics
 		readabilityMetrics = outputModel.readabilityMetrics
+
 		# Text Category and Description
 		request.session['textCategory'] = outputModel.getCategory()
 		request.session['textPurpose'] = outputModel.getDescription()
+
 		# Core Metrics
 		request.session['totalWords'] = coreMetrics.getTotalWords()
 		request.session['totalSentences'] = coreMetrics.getTotalSentences()
@@ -28,6 +53,7 @@ class UserInputPageView(View):
 		request.session['totalEasyWords'] = coreMetrics.getTotalEasyWords()
 		request.session['totalSyllables'] = coreMetrics.getTotalSyllables()
 		request.session['totalCharacters'] = coreMetrics.getTotalCharacters()
+
 		# Readability Metrics
 		request.session['fresValue'] = readabilityMetrics.getFRES().roundedValue
 		request.session['fresLabel'] = readabilityMetrics.getFRES().label
@@ -47,6 +73,11 @@ class UserInputPageView(View):
 		request.session['fryLabel'] = readabilityMetrics.getFRY().label
 
 	def post(self, request):
+		"""
+		The function to handle the HTTP POST request for text input page.
+		:param request: The django HTTP request object
+		:return The redirect object
+		"""
 		contentString = request.POST.get('text_content', None)
 		if contentString is not None:
 			outputModel = Lisibilite(contents=contentString).outputModel
@@ -60,14 +91,29 @@ class UserInputPageView(View):
 class DisplayScoresPageView(View):
 
 	def generateMetricsDict(self, request):
+		"""
+		The function to retreive the output metrics from the session object.
+		:param request: The django HTTP request object
+		:return The dictionary object containing the metrics.
+		"""
 		metricsDict = {}
 		for key in request.session.keys():
 			metricsDict[key] = request.session.get(key)
 		return metricsDict
 
 	def get(self, request):
+		"""
+		The function to handle the HTTP GET request for metrics display.
+		:param request: The django HTTP request object
+		:return The render object
+		"""
 		metricsDict = self.generateMetricsDict(request)
 		return render(request, 'displayscorespage.html', metricsDict)
 
 	def post(self, request):
+		"""
+		The function to handle the HTTP POST request for metrics display.
+		:param request: The django HTTP request object
+		:return The render object
+		"""
 		return render(request, 'displayscorespage.html')
